25028410ca4c6eaa67e9a80e5cc0cb5c7a0d4aa6
张腾 <zhangteng@ZTs-MacBook.local>
2025-02-14 01:13:21 +0800

    project 1

--- proj0/game2048/Model.java
+++ proj0/game2048/Model.java
@@ -138,6 +138,13 @@ public class Model extends Observable {
      * */
     public static boolean emptySpaceExists(Board b) {
         // TODO: Fill in this function.
+        for(int col = 0; col < b.size(); col += 1 ){
+            for (int row = 0; row < b.size(); row += 1){
+                if(b.tile(col, row)==null){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
@@ -148,6 +155,14 @@ public class Model extends Observable {
      */
     public static boolean maxTileExists(Board b) {
         // TODO: Fill in this function.
+        for(int col = 0; col < b.size(); col += 1 ){
+            for (int row = 0; row < b.size(); row += 1){
+                if(b.tile(col, row) != null)
+                if(b.tile(col, row).value() == MAX_PIECE){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
